200fb5fcc86b44894c196a69526d0756ed7eb65d
Nikolay Gelemerov <nikolaygelemerov@gmail.com>
2022-11-15 13:44:32 +0200

    Update npm packages

--- server/server.js
+++ server/server.js
@@ -19,59 +19,62 @@ const app = express();
 const MONGO_URI =
   'mongodb+srv://deanhall:deanhall@cluster0.5boem.mongodb.net/?retryWrites=true&w=majority';
 
-// Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
-mongoose.Promise = global.Promise;
+const start = async () => {
+  // Connect with mongo DB
+  try {
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
 
-// Connect to the mongoDB instance and log a message
-// on success or failure
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-mongoose.connection
-  .once('open', () => console.log('Connected to MongoLab instance.'))
-  .on('error', (error) => console.log('Error connecting to MongoLab:', error));
+    console.log('Connected to MongoLab instance.');
+  } catch (err) {
+    console.log('Error connecting to MongoLab:', err);
+  }
 
-// Configures express to use sessions.  This places an encrypted identifier
-// on the users cookie.  When a user makes a request, this middleware examines
-// the cookie and modifies the request object to indicate which user made the request
-// The cookie itself only contains the id of a session; more data about the session
-// is stored inside of MongoDB.
-app.use(
-  session({
-    resave: true,
-    saveUninitialized: true,
-    secret: 'aaabbbccc',
-    store: new MongoStore({
-      mongoUrl: MONGO_URI
+  // Configures express to use sessions.  This places an encrypted identifier
+  // on the users cookie.  When a user makes a request, this middleware examines
+  // the cookie and modifies the request object to indicate which user made the request
+  // The cookie itself only contains the id of a session; more data about the session
+  // is stored inside of MongoDB.
+  app.use(
+    session({
+      resave: true,
+      saveUninitialized: true,
+      secret: 'aaabbbccc',
+      store: new MongoStore({
+        mongoUrl: MONGO_URI
+      })
     })
-  })
-);
+  );
 
-// Passport is wired into express as a middleware. When a request comes in,
-// Passport will examine the request's session (as set by the above config) and
-// assign the current user to the 'req.user' object.  See also servces/auth.js
-app.use(passport.initialize());
-app.use(passport.session());
+  // Passport is wired into express as a middleware. When a request comes in,
+  // Passport will examine the request's session (as set by the above config) and
+  // assign the current user to the 'req.user' object.  See also servces/auth.js
+  app.use(passport.initialize());
+  app.use(passport.session());
 
-// Instruct Express to pass on any request made to the '/graphql' route
-// to the GraphQL instance.
-app.use(
-  '/graphql',
-  graphqlHTTP({
-    schema,
-    graphiql: true
-  })
-);
+  // Instruct Express to pass on any request made to the '/graphql' route
+  // to the GraphQL instance.
+  app.use(
+    '/graphql',
+    graphqlHTTP({
+      schema,
+      graphiql: true
+    })
+  );
 
-// Webpack runs as a middleware.  If any request comes in for the root route ('/')
-// Webpack will respond with the output of the webpack process: an HTML file and
-// a single bundle.js output of all of our client side Javascript
-const compiler = webpack(webpackConfig);
+  // Webpack runs as a middleware.  If any request comes in for the root route ('/')
+  // Webpack will respond with the output of the webpack process: an HTML file and
+  // a single bundle.js output of all of our client side Javascript
+  const compiler = webpack(webpackConfig);
 
-app.use(webpackDevMiddleware(compiler, webpackConfig.devServer));
-app.use(webpackHotMiddleware(compiler));
+  app.use(webpackDevMiddleware(compiler, webpackConfig.devServer));
+  app.use(webpackHotMiddleware(compiler));
 
-app.listen(4000, () => {
-  console.log('Listening');
-});
+  app.listen(4000, () => {
+    console.log('Listening');
+  });
+};
+
+start();
